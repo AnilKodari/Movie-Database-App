249eb2b559edec1dabe16e33cc2b8fe36b201c60
AnilKodari <ishani14e@gmail.com>
2023-12-16 06:31:24 +0000

    update Solution

--- src/component/PopularMovie/index.js
+++ src/component/PopularMovie/index.js
@@ -1,4 +1,99 @@
+// // PopularMovies.js
+// import {Component} from 'react'
+// import PopularMoviesCard from '../PopularMoviescard'
+// import Header from '../Header'
+// import './index.css'
+
+// const apiStatus = [
+//   {id: 'Popular', name: 'popular'},
+//   {id: 'Top Rated', name: 'top_rated'},
+//   {id: 'Upcoming', name: 'upcoming'},
+// ]
+
+// class PopularMovies extends Component {
+//   state = {
+//     moviesList: [],
+//     currentCategory: 'popular',
+//     searchQuery: '',
+//   }
+
+//   componentDidMount() {
+//     this.getMovies()
+//   }
+
+//   getMovies = async () => {
+//     const {currentCategory, searchQuery} = this.state
+//     const baseUrl = 'https://image.tmdb.org/t/p/w500'
+//     const apiKey = 'd9969545f74a22bdc74cdbd4c7ab5a28'
+
+//     let apiUrl
+//     if (searchQuery) {
+//       apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&query=${searchQuery}`
+//     } else {
+//       apiUrl = `https://api.themoviedb.org/3/movie/${currentCategory}?api_key=${apiKey}&language=en-US&page=1`
+//     }
+
+//     const options = {
+//       method: 'GET',
+//     }
+//     const response = await fetch(apiUrl, options)
+
+//     if (response.ok) {
+//       const data = await response.json()
+//       const updatedMovies = data.results.map(movie => ({
+//         id: movie.id,
+//         posterPath: `${baseUrl}${movie.poster_path}`,
+//         title: movie.title,
+//         rating: movie.vote_average,
+//         backdropPath: movie.backdrop_path,
+//       }))
+//       this.setState({
+//         moviesList: updatedMovies,
+//       })
+//     }
+//   }
+
+//   handleCategoryChange = category => {
+//     const matchingStatus = apiStatus.find(status => status.id === category)
+//     if (matchingStatus) {
+//       this.setState({currentCategory: matchingStatus.name}, () => {
+//         this.getMovies()
+//       })
+//     }
+//   }
+
+//   handleSearchChange = event => {
+//     this.setState({searchQuery: event.target.value})
+//   }
+
+//   handleSearchSubmit = () => {
+//     this.getMovies()
+//   }
+
+//   render() {
+//     const {moviesList, searchQuery} = this.state
+//     return (
+//       <>
+//         <Header
+//           onCategoryChange={this.handleCategoryChange}
+//           onSearchChange={this.handleSearchChange}
+//           onSearchSubmit={this.handleSearchSubmit}
+//           searchQuery={searchQuery}
+//         />
+//         <ul className="movie-list">
+//           {moviesList.map(movie => (
+//             <PopularMoviesCard movieDetails={movie} key={movie.id} />
+//           ))}
+//         </ul>
+//       </>
+//     )
+//   }
+// }
+
+// export default PopularMovies
 // PopularMovies.js
+// PopularMovies.js
+
 import {Component} from 'react'
 import PopularMoviesCard from '../PopularMoviescard'
 import Header from '../Header'
@@ -15,6 +110,8 @@ class PopularMovies extends Component {
     moviesList: [],
     currentCategory: 'popular',
     searchQuery: '',
+    currentPage: 1,
+    totalPages: 1,
   }
 
   componentDidMount() {
@@ -22,15 +119,15 @@ class PopularMovies extends Component {
   }
 
   getMovies = async () => {
-    const {currentCategory, searchQuery} = this.state
+    const {currentCategory, searchQuery, currentPage} = this.state
     const baseUrl = 'https://image.tmdb.org/t/p/w500'
     const apiKey = 'd9969545f74a22bdc74cdbd4c7ab5a28'
 
     let apiUrl
     if (searchQuery) {
-      apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&query=${searchQuery}`
+      apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=${currentPage}&query=${searchQuery}`
     } else {
-      apiUrl = `https://api.themoviedb.org/3/movie/${currentCategory}?api_key=${apiKey}&language=en-US&page=1`
+      apiUrl = `https://api.themoviedb.org/3/movie/${currentCategory}?api_key=${apiKey}&language=en-US&page=${currentPage}`
     }
 
     const options = {
@@ -49,6 +146,7 @@ class PopularMovies extends Component {
       }))
       this.setState({
         moviesList: updatedMovies,
+        totalPages: data.total_pages,
       })
     }
   }
@@ -56,9 +154,12 @@ class PopularMovies extends Component {
   handleCategoryChange = category => {
     const matchingStatus = apiStatus.find(status => status.id === category)
     if (matchingStatus) {
-      this.setState({currentCategory: matchingStatus.name}, () => {
-        this.getMovies()
-      })
+      this.setState(
+        {currentCategory: matchingStatus.name, currentPage: 1},
+        () => {
+          this.getMovies()
+        },
+      )
     }
   }
 
@@ -70,8 +171,17 @@ class PopularMovies extends Component {
     this.getMovies()
   }
 
+  handlePageChange = page => {
+    const {currentPage, totalPages} = this.state
+    if (currentPage !== page && page > 0 && page <= totalPages) {
+      this.setState({currentPage: page}, () => {
+        this.getMovies()
+      })
+    }
+  }
+
   render() {
-    const {moviesList, searchQuery} = this.state
+    const {moviesList, searchQuery, currentPage, totalPages} = this.state
     return (
       <>
         <Header
@@ -85,6 +195,26 @@ class PopularMovies extends Component {
             <PopularMoviesCard movieDetails={movie} key={movie.id} />
           ))}
         </ul>
+        <p>{`Page ${currentPage}`}</p>
+
+        <div className="pagination">
+          <button
+            type="button"
+            onClick={() => this.handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Prev
+          </button>
+
+          <p>{currentPage}</p>
+          <button
+            type="button"
+            onClick={() => this.handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        </div>
       </>
     )
   }
