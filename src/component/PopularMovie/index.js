136074732a6413183f41c9ed427a30d681c2ff13
AnilKodari <ishani14e@gmail.com>
2023-12-16 06:40:47 +0000

    update Solution

--- src/component/PopularMovie/index.js
+++ src/component/PopularMovie/index.js
@@ -1,99 +1,3 @@
-// // PopularMovies.js
-// import {Component} from 'react'
-// import PopularMoviesCard from '../PopularMoviescard'
-// import Header from '../Header'
-// import './index.css'
-
-// const apiStatus = [
-//   {id: 'Popular', name: 'popular'},
-//   {id: 'Top Rated', name: 'top_rated'},
-//   {id: 'Upcoming', name: 'upcoming'},
-// ]
-
-// class PopularMovies extends Component {
-//   state = {
-//     moviesList: [],
-//     currentCategory: 'popular',
-//     searchQuery: '',
-//   }
-
-//   componentDidMount() {
-//     this.getMovies()
-//   }
-
-//   getMovies = async () => {
-//     const {currentCategory, searchQuery} = this.state
-//     const baseUrl = 'https://image.tmdb.org/t/p/w500'
-//     const apiKey = 'd9969545f74a22bdc74cdbd4c7ab5a28'
-
-//     let apiUrl
-//     if (searchQuery) {
-//       apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&page=1&query=${searchQuery}`
-//     } else {
-//       apiUrl = `https://api.themoviedb.org/3/movie/${currentCategory}?api_key=${apiKey}&language=en-US&page=1`
-//     }
-
-//     const options = {
-//       method: 'GET',
-//     }
-//     const response = await fetch(apiUrl, options)
-
-//     if (response.ok) {
-//       const data = await response.json()
-//       const updatedMovies = data.results.map(movie => ({
-//         id: movie.id,
-//         posterPath: `${baseUrl}${movie.poster_path}`,
-//         title: movie.title,
-//         rating: movie.vote_average,
-//         backdropPath: movie.backdrop_path,
-//       }))
-//       this.setState({
-//         moviesList: updatedMovies,
-//       })
-//     }
-//   }
-
-//   handleCategoryChange = category => {
-//     const matchingStatus = apiStatus.find(status => status.id === category)
-//     if (matchingStatus) {
-//       this.setState({currentCategory: matchingStatus.name}, () => {
-//         this.getMovies()
-//       })
-//     }
-//   }
-
-//   handleSearchChange = event => {
-//     this.setState({searchQuery: event.target.value})
-//   }
-
-//   handleSearchSubmit = () => {
-//     this.getMovies()
-//   }
-
-//   render() {
-//     const {moviesList, searchQuery} = this.state
-//     return (
-//       <>
-//         <Header
-//           onCategoryChange={this.handleCategoryChange}
-//           onSearchChange={this.handleSearchChange}
-//           onSearchSubmit={this.handleSearchSubmit}
-//           searchQuery={searchQuery}
-//         />
-//         <ul className="movie-list">
-//           {moviesList.map(movie => (
-//             <PopularMoviesCard movieDetails={movie} key={movie.id} />
-//           ))}
-//         </ul>
-//       </>
-//     )
-//   }
-// }
-
-// export default PopularMovies
-// PopularMovies.js
-// PopularMovies.js
-
 import {Component} from 'react'
 import PopularMoviesCard from '../PopularMoviescard'
 import Header from '../Header'
@@ -195,25 +99,31 @@ class PopularMovies extends Component {
             <PopularMoviesCard movieDetails={movie} key={movie.id} />
           ))}
         </ul>
-        <p>{`Page ${currentPage}`}</p>
 
         <div className="pagination">
-          <button
-            type="button"
-            onClick={() => this.handlePageChange(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            Prev
-          </button>
+          <div>
+            <p>{`Page ${currentPage}`}</p>
+          </div>
+          <div className="button-container">
+            <button
+              type="button"
+              className="page-button"
+              onClick={() => this.handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              Prev
+            </button>
 
-          <p>{currentPage}</p>
-          <button
-            type="button"
-            onClick={() => this.handlePageChange(currentPage + 1)}
-            disabled={currentPage === totalPages}
-          >
-            Next
-          </button>
+            <p className="page">{currentPage}</p>
+            <button
+              type="button"
+              className="page-button"
+              onClick={() => this.handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </>
     )
